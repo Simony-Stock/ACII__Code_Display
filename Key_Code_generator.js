082c0f1f4bc37e93723cbcf54a30eaa2b3044946
Simony Stock <stock.simony@gmail.com>
2020-10-11 06:34:55 -0400

    reset

--- Key_Code_generator.js
+++ Key_Code_generator.js
@@ -1,12 +1,3 @@
 document.addEventListener('DOMContentLoaded', () =>{
-
-const inputDisplay = document.querySelector('#inputDisplay');
-
-document.addEventListener('keyCode', function(event) {
-  if(event.keyCode == 37){
-    inputDisplay.innerHTML = "hello";
-  }
-  
-});
   
 });
