c8376e8cebf6f140676f3d894f68093c81d5dbf7
Simony Stock <stock.simony@gmail.com>
2020-11-06 22:03:58 -0500

    added input and output displays

--- Key_Code_generator.js
+++ Key_Code_generator.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () =>{
+  const input = document.querySelector('#inputDisplay');
+  const output = document.querySelector('#outputDisplay');
+  
+  //getting button inputd
+  document.body.addEventListener('keyup', function(event) {
+      input.innerHTML = event.key;
+      output.innerHTML = "Key code: " + event.code + "\nUnicode: " + event.keyCode ;
+  });
   
 });
